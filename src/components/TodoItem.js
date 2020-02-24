59cfba56f95e2843845fae5765e0a5237ae5b646
Ashley Field <ashleyfield@xtra.co.nz>
2020-02-25 07:56:35 +1300

    Initial Commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';;
+
+export class TodoItem extends React.Component {
+
+	getStyle = () => {
+		return {
+			backgroundColor: '#f4f4f4',
+			padding: '10px',
+			borderBottom: '1px #ccc dotted',
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+		}
+	}
+
+	render() {
+	
+		const { id, title } = this.props.todo; // this line makes it so that when you want to call the property you dont have to type this.props.blah.id every time, the individual property can be called.
+
+		return (
+			<div style={this.getStyle()}>
+				<p>
+					<input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+					{title}
+				</p>
+			</div>
+		)
+	}
+}
+
+// PropTypes
+TodoItem.propTypes = {
+	todo: PropTypes.object.isRequired
+};
+
+export default TodoItem
